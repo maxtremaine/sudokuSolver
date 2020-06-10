44a41b1c1dba3beace453f4f301eb5170535aa79
Max Tremaine <max.tremaine@gmail.com>
2020-06-10 16:42:48 -0400

    Started filling in structs.

--- Package.swift
+++ Package.swift
@@ -14,12 +14,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "sudokuSolver",
-            dependencies: ["Puzzle", "IndexableString"]),
+            dependencies: ["Puzzle", "intIndexableString", "Cell"]),
         .target(
             name: "Puzzle",
-            dependencies: []),
+            dependencies: ["intIndexableString"]),
         .target(
-            name: "IndexableString",
+            name: "Cell",
+            dependencies: ["intIndexableString"]),
+        .target(
+            name: "intIndexableString",
             dependencies: []),
         .testTarget(
             name: "sudokuSolverTests",
--- Sources/sudokuSolver/main.swift
+++ Sources/sudokuSolver/main.swift
@@ -1,9 +1,10 @@
 import Foundation
 import Puzzle
-import IndexableString
+import intIndexableString
+import Cell
 
 // Puzzle.hallo()
 
-let path = "IO/test.txt"
-let contents = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
+let inputPath = "IO/start.sudoku"
+let contents = try String(contentsOfFile: inputPath, encoding: String.Encoding.utf8)
 print(contents[4])
